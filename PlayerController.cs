91a3b7141391d561b3c4af7932566fc00ed4214d
Alvaro Orrego <oravla135@gmail.com>
2019-03-13 15:42:53 -0600

    Agregando todos los archivos

---
+++ PlayerController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController instance; //Singleton
+
+    public float jumpForce = 6f;
+    public float runningSpeed = 1.5f;
+    public Animator animator;
+
+    private Rigidbody2D rigidBody;
+    private Vector3 startingPosition;
+
+    void Awake()
+    {
+        instance = this; //Singleton
+        rigidBody = GetComponent<Rigidbody2D>();
+        startingPosition = this.transform.position;
+    }
+
+    public void StartGame()
+    {
+        animator.SetBool("isAlive", true);
+        this.transform.position = startingPosition;
+    }
+
+    void Update()
+    {
+        if (GameManager.instance.currentGameState == GameState.inGame)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Jump(); 
+            }
+
+            animator.SetBool("isGrunded", IsGrounded());
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (GameManager.instance.currentGameState == GameState.inGame)
+        {
+            if (rigidBody.velocity.x < runningSpeed)
+            {
+                rigidBody.velocity = new Vector2(runningSpeed, rigidBody.velocity.y);
+            }
+        }
+    }
+
+    void Jump()
+    {
+        if(IsGrounded())
+        {
+            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    public LayerMask groundLayer;
+
+    bool IsGrounded()
+    {
+        if (Physics2D.Raycast(this.transform.position, Vector2.down, 0.2f, groundLayer.value))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void Kill()
+    {
+        GameManager.instance.GameOver();
+        animator.SetBool("isAlive", false);
+
+        //nuevo highscore!
+        if(PlayerPrefs.GetFloat("highscore", 0) < this.GetDistance())
+        {
+            PlayerPrefs.SetFloat("highscore", this.GetDistance());
+        }
+    }
+
+    public float GetDistance()
+    {
+        float traveledDistance=Vector2.Distance(
+            new Vector2(startingPosition.x,0),
+            new Vector2(this.transform.position.x,0));
+        return traveledDistance;
+    }
+}   
