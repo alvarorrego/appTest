ad5391634c881bfb5af6f391fb24085e9b2c62e5
Alvaro Orrego <oravla135@gmail.com>
2019-03-13 15:42:53 -0600

    Agregando tofos los archivos

---
+++ LevelPiece.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelPiece : MonoBehaviour
+{
+    public Transform exitPoint;
+}
---
+++ ViewInGame.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ViewInGame : MonoBehaviour
+{
+    public Text scoreLabel;
+    public Text coinLabel;
+    public Text highscoreLabel;
+
+    void Update()
+    {
+        if(GameManager.instance.currentGameState == GameState.inGame)
+        {
+            scoreLabel.text = PlayerController.instance.GetDistance().ToString("f0");
+            coinLabel.text = GameManager.instance.colledtedCoins.ToString();
+            highscoreLabel.text = PlayerPrefs.GetFloat("highscore", 0).ToString("f0");
+        }
+    }
+}
